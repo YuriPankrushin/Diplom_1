4ca981d9579fdfe38d8b413fc1c4eae28d98bf53
Yuri Pankrushin <pankrushinyuri@mail.ru>
2023-03-09 11:58:00 +0300

    Refactor tests in BurgerTest

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
@@ -17,74 +19,88 @@ public class BurgerTest {
     @Mock
     Ingredient ingredient;
 
+    @Mock
+    Database dateBase;
+
+
     @Test
-    public void checkThatIngredientsAdded() {
+    public void checkSetBunsGetNameMethod() {
         Burger burger = new Burger();
         Mockito.when(bun.getName()).thenReturn("black bun");
+        burger.setBuns(bun);
+        Assert.assertEquals("black bun", bun.getName());
+    }
+
+    @Test
+    public void checkSetBunsGetPriceMethod() {
+        Burger burger = new Burger();
         Mockito.when(bun.getPrice()).thenReturn(100f);
         burger.setBuns(bun);
+        Assert.assertEquals(100f, bun.getPrice(), 0);
+    }
 
+    @Test
+    public void checkAddIngredientMethod() {
+        Burger burger = new Burger();
         Mockito.when(ingredient.getType()).thenReturn(IngredientType.FILLING);
         Mockito.when(ingredient.getName()).thenReturn("cutlet");
         Mockito.when(ingredient.getPrice()).thenReturn(100f);
         burger.addIngredient(ingredient);
-
-        Assert.assertEquals("Полученный чек не соответствует ожидаемому",
-                String.format("(==== %s ====)%n" +
-                                "= %s %s =%n" +
-                                "(==== %s ====)%n" +
-                                "%n" +
-                                "Price: %f%n",
-                bun.getName(), ingredient.getType().toString().toLowerCase(),
-                        ingredient.getName(), bun.getName(), burger.getPrice()),
-                burger.getReceipt());
+        Assert.assertEquals(IngredientType.FILLING, ingredient.getType());
+        Assert.assertEquals("cutlet", ingredient.getName());
+        Assert.assertEquals(100f, ingredient.getPrice(), 0);
     }
 
     @Test
-    public void checkThatIngredientsRemoved() {
+    public void checkRemoveIngredientMethod() {
         Burger burger = new Burger();
-        Mockito.when(bun.getName()).thenReturn("black bun");
-        Mockito.when(bun.getPrice()).thenReturn(100f);
-        burger.setBuns(bun);
         burger.addIngredient(ingredient);
         burger.removeIngredient(0);
-
-        Assert.assertEquals("Полученный чек не соответствует ожидаемому",
-                String.format("(==== %s ====)%n" +
-                                "(==== %s ====)%n" +
-                                "%n" +
-                                "Price: %f%n",
-                        bun.getName(), bun.getName(), burger.getPrice()),
-                burger.getReceipt());
+        Assert.assertEquals(0, burger.ingredients.size());
     }
 
     @Test
-    public void checkThatIngredientsMoved() {
+    public void checkMoveIngredientMethod() {
+        Burger burger = new Burger();
+        Mockito.when(dateBase.availableIngredients()).thenReturn(List.of(
+                new Ingredient(IngredientType.SAUCE, "hot sauce", 100),
+                new Ingredient(IngredientType.SAUCE, "sour cream", 200)));
+        burger.addIngredient(dateBase.availableIngredients().get(0));
+        burger.addIngredient(dateBase.availableIngredients().get(1));
+        burger.moveIngredient(0, 1);
+        Assert.assertEquals("hot sauce", burger.ingredients.get(1).getName());
+    }
+
+    @Test
+    public void checkGetPriceMethod() {
+        Burger burger = new Burger();
+        Mockito.when(bun.getPrice()).thenReturn(100f);
+        burger.setBuns(bun);
+        Mockito.when(dateBase.availableIngredients()).thenReturn(List.of(
+                new Ingredient(IngredientType.SAUCE, "hot sauce", 100),
+                new Ingredient(IngredientType.SAUCE, "sour cream", 200)));
+        burger.addIngredient(dateBase.availableIngredients().get(0));
+        burger.addIngredient(dateBase.availableIngredients().get(1));
+        Assert.assertEquals(500f, burger.getPrice(), 0);
+    }
+
+    @Test
+    public void checkGetReceiptMethod() {
         Burger burger = new Burger();
         Mockito.when(bun.getName()).thenReturn("black bun");
         Mockito.when(bun.getPrice()).thenReturn(100f);
         burger.setBuns(bun);
-
-        Mockito.when(ingredient.getType()).thenReturn(IngredientType.FILLING);
-        Mockito.when(ingredient.getName()).thenReturn("cutlet");
-        Mockito.when(ingredient.getPrice()).thenReturn(100f);
-        burger.addIngredient(ingredient);
-        Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
-        Mockito.when(ingredient.getName()).thenReturn("chili sauce");
-        Mockito.when(ingredient.getPrice()).thenReturn(300f);
-        burger.addIngredient(ingredient);
-        burger.moveIngredient(0, 1);
-
-        Assert.assertEquals("Полученный чек не соответствует ожидаемому",
-                String.format("(==== %s ====)%n" +
-                                "= %s %s =%n" +
-                                "= %s %s =%n" +
-                                "(==== %s ====)%n" +
-                                "%n" +
-                                "Price: %f%n",
-                        bun.getName(), ingredient.getType().toString().toLowerCase(),
-                        ingredient.getName(), ingredient.getType().toString().toLowerCase(),
-                        ingredient.getName(), bun.getName(), burger.getPrice()),
-                burger.getReceipt());
+        Mockito.when(dateBase.availableIngredients()).thenReturn(List.of(
+                new Ingredient(IngredientType.SAUCE, "hot sauce", 100),
+                new Ingredient(IngredientType.SAUCE, "sour cream", 200)));
+        burger.addIngredient(dateBase.availableIngredients().get(0));
+        burger.addIngredient(dateBase.availableIngredients().get(1));
+        Assert.assertEquals("(==== black bun ====)\n" +
+                                "= sauce hot sauce =\n" +
+                                "= sauce sour cream =\n" +
+                                "(==== black bun ====)\n" +
+                                "\n" +
+                                "Price: 500.000000\n",
+                                burger.getReceipt());
     }
 }
